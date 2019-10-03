00f2033fb590892744d6e1584667bb16f5c550c1
ddurio2 <ddurio3@gmail.com>
2019-10-02 22:58:22 -0500

    ProMage: - Add new RoundUpToInt

--- Code/Game/MapGen/GenSteps/MGS_RoomsAndPaths.cpp
+++ Code/Game/MapGen/GenSteps/MGS_RoomsAndPaths.cpp
@@ -293,8 +293,8 @@ void MGS_RoomsAndPaths::GeneratePaths( Map& theMap, std::vector<IntVec2>& roomCe
             int absRemainX = abs( remainingDistance.x );
             int absRemainY = abs( remainingDistance.y );
 
-            int pathSizeX = ClampInt( (int)ceil(subPathX), -absRemainX, absRemainX );
-            int pathSizeY = ClampInt( (int)ceil(subPathY), -absRemainY, absRemainY );
+            int pathSizeX = ClampInt( RoundUpToInt(subPathX), -absRemainX, absRemainX );
+            int pathSizeY = ClampInt( RoundUpToInt(subPathY), -absRemainY, absRemainY );
             IntVec2 pathSize( pathSizeX, pathSizeY );
 
             ChangePathTiles( theMap, subPathStart, pathSize );
